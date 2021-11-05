4f1f219a28570e0bb614441da5b281fd8c4ecc70
ShoaibAli1122 <49657823+ShoaibAli1122@users.noreply.github.com>
2021-11-05 10:19:57 +0500

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -1,6 +1,6 @@
 /** 
  * ===================================================================
- * javascript plugins
+ * javascript pluginsdfsdfdsfdsfsdfdsfdsfdsfdsfsdfsdfsdfsdfdsf
  *
  * ------------------------------------------------------------------- 
  */
